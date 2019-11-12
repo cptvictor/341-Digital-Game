1581e8fdbf715c507e385be129b8e3c2c7fa3d42
Victor Liu <vsl27@drexel.edu>
2019-11-12 05:42:42 -0500

    Script for player input. Interacts with char controls

---
+++ 341 Digital Game/Assets/Scenes/PlayerInput.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private CharacterControls controlActions;
+
+    [SerializeField]
+    private GameObject characterCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlActions = GetComponent<CharacterControls>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        controlActions.MoveChar(moveInput);
+
+        Vector2 lookInput = new Vector2(Input.GetAxis("MouseX"), Input.GetAxis("MouseY"));
+
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            controlActions.Crouch();
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            controlActions.Prone();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controlActions.Stand();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            controlActions.changeRunState(true);
+            controlActions.Stand();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            controlActions.changeRunState(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            GameManager.Instance().chooseNewSoldier(this.gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        characterCamera.SetActive(true);
+    }
+
+    void OnDisable()
+    {
+        characterCamera.SetActive(false);
+    }
+}
