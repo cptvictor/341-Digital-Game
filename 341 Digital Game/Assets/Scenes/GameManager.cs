0a9f6d46478a15b6ab5a0ff4d547ae0454ff31c1
Victor Liu <vsl27@drexel.edu>
2019-11-12 05:41:19 -0500

    Game Manager for storing list of soldiers and other general info

---
+++ 341 Digital Game/Assets/Scenes/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    [SerializeField]
+    private GameObject[] soldierList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+            instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static GameManager Instance()
+    {
+        return instance;
+    }
+
+    public void chooseNewSoldier(GameObject oldSoldier)
+    {
+        oldSoldier.GetComponent<PlayerInput>().enabled = false;
+
+        int randSoldier = Mathf.RoundToInt(Random.Range(0, soldierList.Length-1));
+        GameObject newSoldier = soldierList[randSoldier];
+        while(newSoldier == oldSoldier)
+        {
+            randSoldier = Mathf.RoundToInt(Random.Range(0, soldierList.Length-1));
+            newSoldier = soldierList[randSoldier];
+        }
+
+        newSoldier.GetComponent<PlayerInput>().enabled = true;
+    }
+}
