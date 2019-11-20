3e148499a898c03f5d6a42df5d55f24424662382
Victor Liu <vsl27@drexel.edu>
2019-11-20 00:57:10 -0500

    New bullet prefab and placeholder color

---
+++ 341 Digital Game/Assets/Bullet/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 forwardMovement = transform.forward * speed * Time.deltaTime;
+        transform.position += forwardMovement;
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        CharacterControls soldier = other.gameObject.GetComponent<CharacterControls>();
+        if(soldier != null)
+        {
+            soldier.Die();
+        }
+        Destroy(this.gameObject);
+    }
+
+    public void setSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
