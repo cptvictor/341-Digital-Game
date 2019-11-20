443763a71e154eb83d041aa7a5fd6002c23c155b
Victor Liu <vsl27@drexel.edu>
2019-11-20 01:17:08 -0500

    new machine gun prefab and code

---
+++ 341 Digital Game/Assets/Machine Gun/Scripts/MachineGun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGun : MonoBehaviour
+{
+    [SerializeField]
+    private float startAngle = -45f;
+
+    [SerializeField]
+    private float endAngle = 45f;
+
+    public GameObject bullet;
+
+    [SerializeField]
+    private float rateOfFire = 5f;
+
+    private float fireTimer;
+
+    [SerializeField]
+    private float bulletVelocity = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTimer -= Time.deltaTime;
+        if(fireTimer <= 0)
+        {
+            fireTimer = rateOfFire;
+            Bullet newBullet = Instantiate(bullet, transform.position, transform.rotation).GetComponent<Bullet>();
+            newBullet.setSpeed(bulletVelocity);
+        }
+        
+    }
+}
